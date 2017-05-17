3ff72dcdf6fd4c4e206c3544d155c01aa5edd8dd
shaunreeves <shaun.reeves@southstaffs.ac.uk>
2017-05-17 23:21:37 +0100

    Template added
    The core setup of RocketBlaster

---
+++ scripts/MainGame.js
@@ -0,0 +1,14 @@
+BasicGame.Game = function (game) {};
+
+BasicGame.Game.prototype = {
+
+	create: function () {
+       
+	},
+
+	update: function () {
+		//execute 'createUfo','createLife','moveShip','collisionDetection' function
+		
+	}
+
+};
---
+++ scripts/MainMenu.js
@@ -0,0 +1,13 @@
+BasicGame.MainMenu = function (game) { };
+
+BasicGame.MainMenu.prototype = {
+
+	create: function () {
+
+	},
+
+	update: function () {
+		//	Do some nice funky main menu effect here
+	}
+
+};
