a2ccef7078ec3824f048da8ed63d7f301e203213
wolflint <marcinwolf363@gmail.com>
2017-05-20 11:15:39 +0100

    Define audio?

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -127,8 +127,8 @@ BasicGame.Game.prototype = {
     cursors = this.input.keyboard.createCursorKeys();
 
     //Load audio into memory, start music
-    bulletAudio = this.add.audio('bullet');
-    explosionAudio = this.add.audio('explosion');
+    bulletAudio = this.add.audio('bullet', 1, true);
+    explosionAudio = this.add.audio('explosion', 1, true);
     music = this.add.audio('music', 1, true);
     music.play('', 0, 1, true);
 
